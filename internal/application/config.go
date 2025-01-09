a55f15df8852e1338737da39e9716300b37e7bf2
Florian Hansen <flhansen.93@gmail.com>
2025-01-10 00:12:20 +0100

    feat: add sslmode to config

--- internal/application/config.go
+++ internal/application/config.go
@@ -7,17 +7,64 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+type SSLMode int
+
+const (
+	SSLModeRequire SSLMode = iota
+	SSLModeDisable
+	SSLModeVerifyCA
+	SSLModeVerifyFull
+)
+
+func (p SSLMode) String() string {
+	switch p {
+	case SSLModeDisable:
+		return "disable"
+	case SSLModeRequire:
+		return "require"
+	case SSLModeVerifyCA:
+		return "verify-ca"
+	case SSLModeVerifyFull:
+		return "verify-full"
+	default:
+		return "unknown"
+	}
+}
+
+func (p *SSLMode) UnmarshalYAML(value *yaml.Node) error {
+	var val string
+	if err := value.Decode(&val); err != nil {
+		return err
+	}
+
+	switch val {
+	case "require":
+		*p = SSLModeRequire
+	case "disable":
+		*p = SSLModeDisable
+	case "verify-ca":
+		*p = SSLModeVerifyCA
+	case "verify-full":
+		*p = SSLModeVerifyFull
+	default:
+		return fmt.Errorf("unknown value for sslmode: %s, allowed values: ['require', 'disable', 'verify-ca', 'verify-full']", val)
+	}
+
+	return nil
+}
+
 type PostgresConfig struct {
-	Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
-	Username string `yaml:"username"`
-	Password string `yaml:"password"`
-	Database string `yaml:"database"`
+	Host     string  `yaml:"host"`
+	Port     int     `yaml:"port"`
+	Username string  `yaml:"username"`
+	Password string  `yaml:"password"`
+	Database string  `yaml:"database"`
+	SSLMode  SSLMode `yaml:"sslmode"`
 }
 
 func (c PostgresConfig) Dsn() string {
-	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
-		c.Username, c.Password, c.Host, c.Port, c.Database)
+	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		c.Username, c.Password, c.Host, c.Port, c.Database, c.SSLMode)
 }
 
 type Config struct {
